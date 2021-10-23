5cc4b94d69f59393106bda35ab510bdd03f7cdbf
David Bascom <david@99grad.de>
2021-10-23 21:11:00 +0200

    Update

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-11.9.99',
-            'nnhelpers' => '1.3.0-0.0.0',
+            'nnhelpers' => '',
         ],
         'conflicts' => [],
         'suggests' => [],
