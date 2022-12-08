50c897e2d21426ac410c01948fdbbe0a63be3369
David Bascom <david@99grad.de>
2022-12-08 15:17:39 +0100

    Fix version

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,11 +11,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '2.0.0',
+    'version' => '2.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '12.1.0-12.9.99',
-            'nnhelpers' => '2.0.1',
+            'nnhelpers' => '2.0.0',
         ],
         'conflicts' => [],
         'suggests' => [],
